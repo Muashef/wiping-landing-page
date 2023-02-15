4d4b32cacc88169b70fa187404df01a2cc4ca0aa
Sherif <muhammedsherif40@yahoo.com>
2023-02-15 18:54:21 +0100

    updated contact form validation

--- src/components/GetInTouch.jsx
+++ src/components/GetInTouch.jsx
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import emailjs from '@emailjs/browser'
 import {BsEmojiSmile} from 'react-icons/bs'
 
 
 
 function GetInTouch() {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm(
+        'service_6a1myxj', 
+        'template_waq87rv', 
+        form.current, 
+        '_5W48x--szIoDwRgD'
+        )
+        .then((result) => {
+            console.log(result.text);
+            console.log('message sent');
+        }, (error) => {
+            console.log(error.text);
+        });
+        e.target.reset()
+    };
+
     return (
         <div className='bg-white py-22 px-4'>
             <div className='w-full h-full flex flex-col items-center justify-center mx-auto '>
                 <h1 className='text-[#2F80ED] text-3xl font-bold text-center mb-2'>Get In Touch</h1>
                 <p className='flex flex-row items-center justify-center text-center text-black font-medium gap-2'>Don't be shy, feel free to reach out to us. <BsEmojiSmile /> </p>
-                <form className='w-[full] lg:w-[70%] mt-10'>
+                <form ref={form} onSubmit={sendEmail} className='w-[full] lg:w-[70%] mt-10'>
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-[1rem]">
-                        <input type="text" placeholder="Name" className='bg-[#EFEFEF] py-3 px-4 outline-none rounded-md md:rounded-none' />
-                        <input type="email" placeholder="Email" className='bg-[#EFEFEF] py-3 px-4 outline-none rounded-md md:rounded-none' />
+                        <input type="text" placeholder="Name" name="user_name" className='bg-[#EFEFEF] py-3 px-4 outline-none rounded-md md:rounded-none' required/>
+                        <input type="email" placeholder="Email" name="user_email" className='bg-[#EFEFEF] py-3 px-4 outline-none rounded-md md:rounded-none' required/>
                         <textarea
                         className="w-full md:col-span-2 bg-[#EFEFEF] outline-none py-2 px-4 rounded-md md:rounded-none"
                         placeholder="Message"
+                        name="message"
+                        required
                         cols="30"
                         rows="10"
                         ></textarea>
